6d47747db4748ae0a7d1f4b45d303662e2e6af39
theblackman2 <paskasonga@gmail.com>
2022-05-03 15:31:30 +0100

    feat: multiple35 résolu

--- multiple35.js
+++ multiple35.js
@@ -1,5 +1,20 @@
 function multiple_3_5(n) {
   // corps de la fonction ici
+
+  let somme = 0
+
+  for(let i = 0; i < n; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+      //le reste de la division entre i et 5 ou 3 donne 0
+      somme += i
+
+    }
+  }
+
+  return somme
+
 }
 
-module.exports = multiple_3_5;
+console.log(multiple_3_5(15))
+
+// module.exports = multiple_3_5;
