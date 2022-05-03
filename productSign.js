51cb5f4353df1dcf4580824e559dc001e8786792
theblackman2 <paskasonga@gmail.com>
2022-05-03 15:21:01 +0100

    feat: productSign résolu

--- productSign.js
+++ productSign.js
@@ -1,7 +1,15 @@
 
 function productSign(n, m) {
   // corps de la fonction ici
-  let a = "test"
+  let product = n * m
+
+  if(product < 0){
+    return "le produit est négatif"
+  }else if(product > 0){
+    return "le produit est positif"
+  }else{
+    return "le produit est nul"
+  }
 }
 
 module.exports = productSign;
