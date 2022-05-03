c99165a79a8bdefb50d0c3196960582ba96b9641
theblackman2 <paskasonga@gmail.com>
2022-05-03 15:17:10 +0100

    feat: n_parite résolu

--- n_parite.js
+++ n_parite.js
@@ -1,5 +1,24 @@
 function nParite(n, parite) {
   // corps de la fonction ici
+  let somme = 0
+  let count = 1
+  if(parite == true){
+    let i = 0
+    while(count <= n){
+      somme += i
+      i += 2
+      count++
+    }
+  }else{
+    let i = 1
+    while(count <= n){
+      somme += i
+      i += 2
+      count++
+    }
+  }
+
+  return somme
 }
 
 module.exports = nParite;
